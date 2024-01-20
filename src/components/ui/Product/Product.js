c52e392e75dda0f0b2ff4b0ac120ea6d43b2652c
Fileto Filho <fileto.filho@voidsoftware.com>
2024-01-20 00:26:52 -0300

    feature: animation

--- src/components/ui/Product/Product.js
+++ src/components/ui/Product/Product.js
@@ -5,9 +5,9 @@ import i18n from "i18next";
 const Product = (props) => {
     const { productName, productPrice, productImage } = props
     return (
-        <div className="flex flex-col justify-center items-center p-10">
+        <div className="flex flex-col justify-center items-center p-10 duration-700 rounded-lg hover:bg-gray-200">
             <div className="h-72 flex items-center">
-                <img className="h-60" src={productImage} />
+                <img className="h-60 duration-500 hover:scale-110" src={productImage} />
             </div>
             <p>{productName}</p>
             <p className="font-bold">R$ {productPrice}</p>
